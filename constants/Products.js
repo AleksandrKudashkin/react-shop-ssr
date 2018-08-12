5516f8a138f944dc025f3d149d7ab136922aed7f
Alex Kudashkin <alex@kudashkin.pro>
2018-08-12 10:38:56 -0400

    Add missing dependencies and update products array

--- constants/Products.js
+++ constants/Products.js
@@ -3,19 +3,19 @@ const Products = [
     id: 1,
     title: 'Assam tea',
     price: 1050,
-    imageUrl: 'https://www.rusteaco.ru/upload/iblock/bf0/bf0a7397462e387d126a692d4f3c161c.jpg'
+    imageUrl: 'http://teapravda.com/wp-content/uploads/chaj-assam-e1483123922628.jpg'
   },
   {
     id: 2,
     title: 'Milk Oolong',
     price: 1790,
-    imageUrl: 'https://www.rusteaco.ru/upload/iblock/3fc/3fc35a10dbc87ee18b25bcba8261429f.jpg'
+    imageUrl: 'https://www.itea.com.au/content/uploads/2018/02/Milk-Oolong-Tea.jpg'
   },
   {
     id: 1,
     title: 'Earl Grey',
     price: 565,
-    imageUrl: 'https://www.rusteaco.ru/upload/iblock/3be/3beaf7a17f16f453d4a21ec635c04830.jpg'
+    imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/5/5f/EarlGrey_mit_Bergamotte_aromatisierter_Schwarzer_Tee.jpg/560px-EarlGrey_mit_Bergamotte_aromatisierter_Schwarzer_Tee.jpg'
   }
 ];
 
