1fb94fb792d52fbebe74649ab3baab57459bce4a
Alex Kudashkin <alex@kudashkin.pro>
2018-08-12 10:31:30 -0400

    Add components

---
+++ constants/Products.js
@@ -0,0 +1,22 @@
+const Products = [
+  {
+    id: 1,
+    title: 'Assam tea',
+    price: 1050,
+    imageUrl: 'https://www.rusteaco.ru/upload/iblock/bf0/bf0a7397462e387d126a692d4f3c161c.jpg'
+  },
+  {
+    id: 2,
+    title: 'Milk Oolong',
+    price: 1790,
+    imageUrl: 'https://www.rusteaco.ru/upload/iblock/3fc/3fc35a10dbc87ee18b25bcba8261429f.jpg'
+  },
+  {
+    id: 1,
+    title: 'Earl Grey',
+    price: 565,
+    imageUrl: 'https://www.rusteaco.ru/upload/iblock/3be/3beaf7a17f16f453d4a21ec635c04830.jpg'
+  }
+];
+
+export default Products;
